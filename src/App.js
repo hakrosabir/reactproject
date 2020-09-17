e32a3bdda4a1dfa3da6904493b844d49fbcf064c
Sabir Hakro <hakrosabir@gmail.com>
2020-09-17 19:07:37 +0500

    Update display message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div>
-     <h1>Hello World from Sabir Hakro! </h1> 
+     <h1>Hi Everyone! Thankyou for visiting us! :) </h1> 
     </div>
   );
 }
